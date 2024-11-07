5b918356433e8423cc30a66972c5100ef4585dec
Scott Moss <willscottmoss@gmail.com>
2024-11-07 10:41:03 -0600

    done

---
+++ evals/experiments/reddit.eval.ts
@@ -0,0 +1,35 @@
+import { runLLM } from '../../src/llm'
+import { redditToolDefinition } from '../../src/tools/reddit'
+import { runEval } from '../evalTools'
+import { ToolCallMatch } from '../scorers'
+
+const createToolCallMessage = (toolName: string) => ({
+  role: 'assistant',
+  tool_calls: [
+    {
+      type: 'function',
+      function: {
+        name: toolName,
+      },
+    },
+  ],
+})
+
+runEval('reddit', {
+  task: (input) =>
+    runLLM({
+      messages: [{ role: 'user', content: input }],
+      tools: [redditToolDefinition],
+    }),
+  data: [
+    {
+      input: 'find me something interesting on reddit',
+      expected: createToolCallMessage(redditToolDefinition.name),
+    },
+    {
+      input: 'hi',
+      expected: createToolCallMessage(redditToolDefinition.name),
+    },
+  ],
+  scorers: [ToolCallMatch],
+})
